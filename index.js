ce9465e899ec190e3aa4ee8c043cd74f65508e0e
dea <108593938+deaamanger@users.noreply.github.com>
2023-11-20 10:55:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+const axios = require("axios");
+const ccxt = require("ccxt");
+const axios = require("axios");
+
+const binanceClient = new ccxt.binance({
+  apiKey: process.env.API_ENV,
+  secret: process.env.API_SECRET,
+});
+
+const tick = async () => {
+  const { asset, base, secret, allocation } = config;
+  const market = `${asset}/${base}`;
+
+  const orders = await binanceClient.fetchOpenOrders(market);
+  orders.forEach(async (order) => {
+    await binanceClient.cancelOrder(order.id);
+  });
+  const results = await Promise.all([
+    axios.get(`https request`),
+    axios.get(` https req change`),
+  ]);
+  const marketPrice = results[0].data.bitcoin.usd / results[1].data.tether.usd;
+
+  const sellPrice = marketPrice * (1 + spread);
+  const buyPrice = marketPrice * (1 - spread);
+  const balances = await binanceClient.fetchBalance();
+  const assetBalance = balances.free(asset);
+  const baseBalance = balances.free(base);
+  const sellVolume = assetBalance * allocation;
+  const buyVolume = (baseBalance * allocation) / marketPrice;
+
+  await binanceClient.createLimitSellOrder(market, sellVolume, sellPrice);
+  await binanceClient.createLimitBuyOrder(market, buyPrice, buyVolume);
+
+  console.log(`
+  New tick for ${market}...
+   Created limmit order for ${sellVolume}@${sellPrice}
+   created limmit buy order ${buyVolume}@${buyPrice}
+   `);
+};
+
+const run = () => {
+  const config = {
+    asset: "BTC",
+    base: "USDT",
+    allocation: 0.1,
+    spread: 0.2,
+    tickInterval: 2000,
+  };
+
+  tick(config, binanceClient);
+  setInterval(tick, config.tickInterval, config, binanceClient);
+};
+
+run();
